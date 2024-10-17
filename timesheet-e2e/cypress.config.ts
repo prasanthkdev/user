605fe47e98656826c80ada04f5e4609b2e260c04
Prasanth.k <prasanth.k@revolite.io>
2024-10-17 16:45:30 +0530

    initial commit

---
+++ timesheet-e2e/cypress.config.ts
@@ -0,0 +1,18 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run timesheet:serve',
+        production: 'npx nx run timesheet:serve',
+      },
+      ciWebServerCommand: 'npx nx run timesheet:serve',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:8081',
+  },
+});
